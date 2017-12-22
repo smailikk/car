eb24babab42ec07c069e76807f9b03ffcf69b007
smailikk <32063140+smailikk@users.noreply.github.com>
2017-12-22 03:29:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -399,33 +399,6 @@
             </div>
         </div>
     </section>
-
-
-    <footer>
-
-        <div class="container">
-            <div class="row">
-                <div class="footer-containertent">
-
-                    <ul class="footer-social-info">
-                        <li>
-                            <a href="#"><i class="fa fa-facebook"></i></a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-twitter"></i></a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-pinterest"></i></a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-google-plus"></i></a>
-                        </li>
-                    </ul>
-					<br/><br/>
-                </div>
-            </div>
-        </div>
-    </footer>
     <!-- Footer End -->
 
     <script src="js/jquery-1.11.3.min.js"></script>
